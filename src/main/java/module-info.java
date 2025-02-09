dd33ecd409d4d745f8075ecf7b3db8a76edddddd
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2025-02-09 13:50:08 +0800

    Add sql library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module application.dormtrackr {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens application.dormtrackr to javafx.fxml;
     exports application.dormtrackr;
