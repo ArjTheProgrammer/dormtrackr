9e96deb8321f04b5d7c397dd13251dd929badba7
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2025-02-09 13:33:10 +0800

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module application.dormtrackr {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens application.dormtrackr to javafx.fxml;
+    exports application.dormtrackr;
+}
