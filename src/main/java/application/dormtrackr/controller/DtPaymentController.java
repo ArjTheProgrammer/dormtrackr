bffa5be3716ee506158728e13308e082c8e1c9d3
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2025-02-16 15:37:25 +0800

    Make make the view table function of payment

--- src/main/java/application/dormtrackr/controller/DtPaymentController.java
+++ src/main/java/application/dormtrackr/controller/DtPaymentController.java
@@ -1,8 +1,12 @@
 package application.dormtrackr.controller;
 
+import application.dormtrackr.model.Payment;
+import application.dormtrackr.model.dao.PaymentDAO;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,16 +14,41 @@ import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class DtPaymentController implements Initializable {
 
+
+    @FXML
+    private TableColumn<Payment, Integer> dormerId;
+
     @FXML
     private Button homeButton;
 
+    @FXML
+    private DatePicker inputDate;
+
+    @FXML
+    private TextField inputDormerId;
+
+    @FXML
+    private TableColumn<Payment, LocalDate> paymentDate;
+
+    @FXML
+    private TableColumn<Payment, Integer> paymentId;
+
+    @FXML
+    private TableView<Payment> paymentTable;
+
+    private PaymentDAO paymentDAO;
+    private int index;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        paymentDAO = new PaymentDAO();
         homeButton.setOnAction(event -> loadScene("/application/dormtrackr/dashboard.fxml"));
+        viewPaymentTable();
     }
 
     private void loadScene(String fxmlFile) {
@@ -33,4 +62,28 @@ public class DtPaymentController implements Initializable {
             e.printStackTrace();
         }
     }
+
+    private void viewPaymentTable() {
+        ObservableList<Payment> payments = paymentDAO.getPayments();
+        System.out.println("Number of payments: " + payments.size());
+        paymentTable.setItems(payments);
+
+        paymentId.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getPaymentId()));
+        dormerId.setCellValueFactory(f -> new ReadOnlyObjectWrapper<>(f.getValue().getDormerId()));
+        paymentDate.setCellValueFactory(f -> f.getValue().paymentDateProperty());
+
+        paymentTable.setRowFactory(tv -> {
+            TableRow<Payment> myRow = new TableRow<>();
+            myRow.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (!myRow.isEmpty())) {
+                    index = paymentTable.getSelectionModel().getSelectedIndex();
+
+                    inputDormerId.setText(String.valueOf(paymentTable.getItems().get(index).getDormerId()));
+                    inputDate.setValue(paymentTable.getItems().get(index).getPaymentDate());
+                }
+            });
+            return myRow;
+        });
+    }
+
 }
--- src/main/java/application/dormtrackr/model/dao/PaymentDAO.java
+++ src/main/java/application/dormtrackr/model/dao/PaymentDAO.java
@@ -1,7 +1,26 @@
 package application.dormtrackr.model.dao;
 
+import application.dormtrackr.model.Payment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
 public class PaymentDAO {
     public String getPaidStudents(){
         return "2/4";
+    }
+
+    public ObservableList<Payment> getPayments(){
+        return FXCollections.observableArrayList(
+                new Payment(1, 1, LocalDate.of(2025, 1, 1)),
+                new Payment(2, 1, LocalDate.of(2025, 2, 1)),
+                new Payment(3, 2, LocalDate.of(2025, 1, 1)),
+                new Payment(4, 2, LocalDate.of(2025, 2, 1)),
+                new Payment(5, 3, LocalDate.of(2025, 1, 1)),
+                new Payment(6, 3, LocalDate.of(2025, 2, 1)),
+                new Payment(7, 4, LocalDate.of(2025, 1, 1)),
+                new Payment(8, 4, LocalDate.of(2025, 2, 1))
+        );
     }
 }
