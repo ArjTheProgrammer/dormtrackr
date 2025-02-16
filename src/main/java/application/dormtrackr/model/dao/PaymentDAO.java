8f25670d0b772749a9accea8bddabec1678c019d
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2025-02-16 22:26:20 +0800

    Change the PaymentDAO to connect to the database

--- src/main/java/application/dormtrackr/model/dao/PaymentDAO.java
+++ src/main/java/application/dormtrackr/model/dao/PaymentDAO.java
@@ -1,27 +1,51 @@
 package application.dormtrackr.model.dao;
 
-import application.dormtrackr.model.Dormer;
 import application.dormtrackr.model.Payment;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.time.LocalDate;
+import java.sql.*;
 
 public class PaymentDAO extends BaseDAO<Payment> {
+    private static final String GET_ALL_PAYMENTS = "SELECT * FROM Payment";
+    private static final String GET_RATIO = "WITH PaidThisMonth AS (SELECT dormer_id FROM Payment WHERE YEAR(payment_date) = YEAR(GETDATE()) AND MONTH(payment_date) = MONTH(GETDATE())) SELECT COUNT(*) AS total_dormers, (SELECT COUNT(*) FROM PaidThisMonth) AS paid_dormers FROM Dormer";
+
     public String getPaidStudents(){
-        return "2/4";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(GET_RATIO);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                int totalDormers = rs.getInt("total_dormers");
+                int paidDormers = rs.getInt("paid_dormers");
+                return paidDormers + "/" + totalDormers;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "NULL";
     }
 
     public ObservableList<Payment> getPayments(){
-        return FXCollections.observableArrayList(
-                new Payment(1, 1, LocalDate.of(2025, 1, 1)),
-                new Payment(2, 1, LocalDate.of(2025, 2, 1)),
-                new Payment(3, 2, LocalDate.of(2025, 1, 1)),
-                new Payment(4, 2, LocalDate.of(2025, 2, 1)),
-                new Payment(5, 3, LocalDate.of(2025, 1, 1)),
-                new Payment(6, 3, LocalDate.of(2025, 2, 1)),
-                new Payment(7, 4, LocalDate.of(2025, 1, 1)),
-                new Payment(8, 4, LocalDate.of(2025, 2, 1))
-        );
+        ObservableList<Payment> payments = FXCollections.observableArrayList();
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(GET_ALL_PAYMENTS);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                payments.add(mapResultSetToPayments(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return payments;
+    }
+
+    private Payment mapResultSetToPayments(ResultSet rs) throws SQLException {
+        return new Payment(
+                rs.getInt("payment_id"),
+                rs.getInt("dormer_id"),
+                rs.getDate("payment_date").toLocalDate());
     }
 }
